dda02af5028ba576e8798170d7c6b1e8fdc30bf0
syifaamdh <syifaamdh@gmail.com>
2025-05-04 23:21:43 +0700

    Initial commit with vulnerable application

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import subprocess
+
 def greet(name):
-print(f"Hello, {name}!")
+    print(f"Hello, {name}!")
+
 def run_command(cmd):
-subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True)
+
 if __name__ == "__main__":
-name = input("Enter your name: ")
-greet(name)
-cmd = input("Enter a command to run: ")
-run_command(cmd)
+    name = input("Enter your name: ")
+    greet(name)
+    cmd = input("Enter a command to run: ")
+    run_command(cmd)
